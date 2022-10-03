4f7298edc73174fc9d979705d1ef5a0b2c875ea3
GOWTHAM KUMAR V <vgowtham.kumar8@gmail.com>
2022-10-03 12:41:41 +0530

    final

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -18,7 +18,7 @@ export default function Contact() {
             </section>
             <div className="parent">
             <div className="email">
-              <div className="round"><MdLocationOn className="mdl" size={40} color="rgb(65, 201, 255)" /></div>
+              <div className="round" href="https://www.google.com/maps/place/Virudhunagar,+Tamil+Nadu/@9.5734414,77.9581904,13z/data=!4m13!1m7!3m6!1s0x3b012c93bcb2b013:0xd717404a42ed026e!2sVirudhunagar,+Tamil+Nadu!3b1!8m2!3d9.5680116!4d77.9624435!3m4!1s0x3b012c93bcb2b013:0xd717404a42ed026e!8m2!3d9.5680116!4d77.9624435"><MdLocationOn  className="mdl" size={40} color="rgb(65, 201, 255)" /></div>
               <p className="loca">Location</p>
               <p className="sub" style={{font:"small-caption"}}>Virudhunagar, Tamilnadu.</p>
               </div>
